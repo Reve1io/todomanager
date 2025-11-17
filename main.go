cb41fd770df0aca04c3f77abb1d1440121e91574
Reve1io <povezlo.tak@mail.ru>
2025-11-17 17:44:52 +0300

    2

--- main.go
+++ main.go
@@ -12,9 +12,9 @@ type Task struct {
 	Done  bool   `json:"done"`
 }
 
-func (task Task) loadTasks() []Task {
+func loadTasks() []Task {
 
-	filepath := "D://go//TodoList/tasks.json"
+	filepath := "D:/go/TodoList/tasks.json"
 
 	if _, err := os.Stat(filepath); os.IsNotExist(err) {
 		fmt.Printf("Файл '%s' не существует", filepath)
@@ -24,6 +24,7 @@ func (task Task) loadTasks() []Task {
 	fileRead, err := os.ReadFile(filepath)
 	if err != nil {
 		fmt.Printf("Ошибка чтения файла: %v", err)
+		return []Task{}
 	}
 
 	if len(fileRead) == 0 {
@@ -32,29 +33,44 @@ func (task Task) loadTasks() []Task {
 
 	var tasks []Task
 	if err := json.Unmarshal(fileRead, &tasks); err != nil {
-		return nil
+		return []Task{}
 	}
 
 	return tasks
 }
 
-func (task Task) add() {
+func (task *Task) add() {
+	tasks := loadTasks()
 
-	jsonTask, err := json.MarshalIndent(task, "", " ")
-	if err != nil {
-		fmt.Println(err)
-		return
+	newID := tasks[len(tasks)-1].ID + 1
+
+	if len(tasks) == 0 {
+		newID = 1
 	}
 
-	fileWrite := os.WriteFile("tasks.json", jsonTask, 0644)
+	newTask := Task{
+		ID:    newID,
+		Title: os.Args[2],
+		Done:  false,
+	}
+
+	tasks = append(tasks, newTask)
+
+	saveTask(tasks)
+	fmt.Println("Задача добавлена")
+}
+
+func saveTask(tasks []Task) {
+	jsonTask, err := json.MarshalIndent(tasks, "", " ")
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	filepath := "D:/go/TodoList/tasks.json"
+
+	fileWrite := os.WriteFile(filepath, jsonTask, 0644)
 	if fileWrite != nil {
 		fmt.Println("Ошибка при записи файла:", fileWrite)
 	}
 	fmt.Println("Объект успешно записан")
-}
-
-func main() {
-	task := Task{ID: 2, Title: os.Args[2], Done: false}
-	task.add()
-	task.loadTasks()
 }
